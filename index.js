335cd72afa75dd528d975bcefffa5f80ac81c095
Jadyn Koehn <jadynscottkoehn@gmail.com>
2026-03-16 09:07:36 -0500

    ve8

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import PG from 'pg';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -21,8 +26,8 @@ const db = new PG.Client({
 
 //db.connect();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
