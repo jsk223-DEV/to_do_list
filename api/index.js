e36d68992655432bd62d8aa0c98242c00c5d9902
Jadyn Koehn <jadynscottkoehn@gmail.com>
2026-03-16 19:48:01 -0500

    ve15

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const db = new PG.Client({
 
 db.connect();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'));
